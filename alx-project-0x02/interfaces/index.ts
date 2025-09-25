d1d4188bfdaed3d407a520b3c69ac16bf5dbeadc
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-25 06:17:39 +0300

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,1 +1,9 @@
-
+/**
+ * Defines the props structure for the Card component.
+ * The main paragraph content of the card.
+ * The bold title displayed at the top of the card. */
+ */
+export interface CardProps {
+  title: string;
+  content: string;
+}
