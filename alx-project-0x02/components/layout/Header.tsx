3644f6da35768192fa28fcb0756f57762d2e3333
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-25 06:10:37 +0300

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,34 @@
+import Link from 'next/link';
+import React from 'react';
 
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 p-4 text-white flex space-x-5">
+      <nav className="flex space-x-5">
+        
+        {/* Navigates to the root (index) page */}
+        <Link href="/">
+            <span className="text-white hover:text-blue-400 cursor-pointer transition duration-300">
+                Root
+            </span>
+        </Link>
+        
+        {/* Navigates to the /home page */}
+        <Link href="/home">
+            <span className="text-white hover:text-blue-400 cursor-pointer transition duration-300">
+                Home
+            </span>
+        </Link>
+
+        {/* Navigates to the /about page */}
+        <Link href="/about">
+            <span className="text-white hover:text-blue-400 cursor-pointer transition duration-300">
+                About
+            </span>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
